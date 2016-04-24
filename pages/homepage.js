318a1076c95324faad47409fcbc9afed5f93845e
Apoorv <apoorv.1591@gmail.com>
2016-04-24 17:50:38 +0530

    page objects model with sample test case'

---
+++ pages/homepage.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+	url: 'http://www.shutterstock.com',
+	
+	elements : {
+
+	searchbutton: 'span[class="input-group-btn"] button[type="submit"]'
+	}
+};
--- tests/sample.js
+++ tests/sample.js
@@ -1,6 +1,23 @@
 module.exports = {
-  'Demo test Google' : function (browser) {
+  '@tags': ['run'],
+'Tests 1' : function (browser) {
     browser
+      .windowMaximize()
+      .url('http://www.google.com')
+       .waitForElementVisible('body', 1000)
+      .setValue('input[type=text]', 'nightwatch')
+      .waitForElementVisible('button[name=btnG]', 1000)
+      .click('button[name=btnG]')
+      .pause(1000)
+      .assert.containsText('#main', 'Night Watch')
+      .end();
+
+    },
+
+    '@tags': ['second'],
+
+    'Test 2' : function (browser) {
+      browser
       .windowMaximize()
       .url('http://www.google.com')
       .waitForElementVisible('body', 1000)
@@ -10,5 +27,14 @@ module.exports = {
       .pause(1000)
       .assert.containsText('#main', 'Night Watch')
       .end();
+  },
+
+
+  'Test 3' :  function(client) {
+    
+    client.windowMaximize()
+   client.page.homepage().navigate();
+    client.page.homepage().click('@searchbutton');
+    client.end();
   }
 };
